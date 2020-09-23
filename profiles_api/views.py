2ec2bd0e796cb31001e32254b71eba4e273a5e20
ETursunov <eldor1999@protonmail.com>
2020-09-23 19:12:11 +0500

    fixed bugs

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,5 @@
-from rest_restframework.views import APIView
-from rest_restframework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class HelloApiView(APIView):
 	""" Test API View """
